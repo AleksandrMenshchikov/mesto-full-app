ccdd3ab3757bcc22ca7a0a1eef6a24e0ec399c3b
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2024-05-21 19:37:12 +0300

    fix ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,6 +24,7 @@ module.exports = {
       'ref': DEPLOY_REF,
       'repo': DEPLOY_REPO,
       'path': DEPLOY_PATH,
+      'pre-deploy': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm ci && npm run build'
     }
   }
